471e2b216fc8d9d8a84239f24222171162132a11
unknown <lspardo@uniboyaca.edu.co>
2023-04-04 23:29:42 -0500

    se realizo el tercer ejercicio del taller

---
+++ js/excercise3.js
@@ -0,0 +1,11 @@
+document.getElementById('accion').addEventListener('click',()=>{
+    let nomPer = document.getElementById('nombre').value;
+    let edadPer = document.getElementById('edad').value;
+    if(edadPer>=18){
+        document.getElementById('output').innerText = 'Hola '+nomPer+',eres mayor de edad';
+    }else if(edadPer<18 && edadPer>=0){
+        document.getElementById('output').innerText = 'Hola '+nomPer+',no eres mayor de edad';    
+    }else{
+        document.getElementById('output').innerText = 'Hola '+nomPer+',ingresa una edad valida';
+    }
+})
